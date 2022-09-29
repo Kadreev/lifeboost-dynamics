0fdc3cb0e1d56f0a5d8319c657602b799bd12643
Kadreev <kadreev@gmail.com>
2022-09-30 02:39:11 +0300

    3 files modified

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -18,5 +19,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), visualizer()],
+  plugins: [vue(), visualizer(), vueJsx({
+    // options are passed on to @vue/babel-plugin-jsx
+  })],
 });
