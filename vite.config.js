74c9e6b2c6a99a311acde6ba5b2b7ecddf68005d
Kadreev <kadreev@gmail.com>
2022-09-30 02:29:51 +0300

    3 files modified

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -18,5 +18,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), visualizer(), visualizer()],
 });
