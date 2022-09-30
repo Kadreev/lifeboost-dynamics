e16d23ee5d9199a8324d6ab5a42e9c7572028dd4
Kadreev <kadreev@gmail.com>
2022-09-30 03:56:21 +0300

    3 files modified, 1 added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -15,11 +15,15 @@ export default defineConfig({
         preserveModules: false,
         // Provide global variables to use in the UMD build
         // for externalized deps
-        manualChunks: () => 'main.js',
+        manualChunks: () => 'main.jsx',
       },
     },
   },
-  plugins: [vue(), visualizer(), vueJsx({
-    // options are passed on to @vue/babel-plugin-jsx
-  })],
+  plugins: [
+    vue(),
+    visualizer(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
+  ],
 });
